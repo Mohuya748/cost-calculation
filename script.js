30c08742d2644b73fd1fb94669593227174819b6
Mohuya748 <khandakermohuya@gmail.com>
2022-02-17 14:46:47 +0600

    function added

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-let incomeInput = document.getElementById("income-input");
-let foodInput = document.getElementById("food-input");
-let rentInput = document.getElementById("rent-input");
-let clothsInput = document.getElementById("cloths-input");
+// function for all input values calling
+function inputValue(inputId) {
+    let input = document.getElementById(inputId);
+    return parseFloat(input.value);
+}
+
 let totalExpenses = document.getElementById("total-expenses");
 let balance = document.getElementById("balance");
-let saveInput = document.getElementById("save-input");
 let savingAmount = document.getElementById("saving-amount");
 let lastBalance = document.getElementById("remaining-balance");
 const errorMsg = document.getElementById("error-msg");
@@ -22,41 +23,38 @@ function minusValue(firstValue, secondValue) {
 }
 //--------------calculate button -----------
 function calculate() {
-    if (incomeInput.value>0 && foodInput.value > 0 && rentInput.value>0 && clothsInput.value>0) {
-        errorMsg.style.display = 'none';  
-        let expenses = addValue(foodInput.value, rentInput.value, clothsInput.value);
+    if (inputValue("income-input") > 0 && inputValue("food-input") > 0 && inputValue("rent-input") > 0 && inputValue("cloths-input") > 0) {
+        errorMsg.style.display = 'none';
+        let expenses = addValue(inputValue("food-input"), inputValue("rent-input"), inputValue("cloths-input"));
         totalExpenses.innerText = expenses;
-        let leftBalance = minusValue(incomeInput.value, totalExpenses.innerText);
+        let leftBalance = minusValue(inputValue("income-input"), totalExpenses.innerText);
         if (leftBalance > 0) {
             balance.innerText = leftBalance;
         }
         else {
             alert("expenses can't be greater than income. Please give a valid input in each field.");
             totalExpenses.innerText = 0;
+            balance.innerText = 0;
         }
     }
     else {
         errorMsg.style.display = 'block';
-        incomeInput.value = "";
-        foodInput.value = "";
-        rentInput.value = "";
-        clothsInput.value = "";
     }
 }
-  
+
 //--------- save button -----------
 function save() {
-    totalSave = parseFloat(incomeInput.value) * (parseFloat(saveInput.value) / 100);
+    totalSave = inputValue("income-input") * (inputValue("save-input") / 100);
     savingAmount.innerText = totalSave.toFixed(2);
-    remainBalance = minusValue(incomeInput.value, addValue(savingAmount.innerText, totalExpenses.innerText, 0));
-    if(totalSave<balance.innerText){
+    remainBalance = minusValue(inputValue("income-input"), addValue(savingAmount.innerText, totalExpenses.innerText, 0));
+    if (totalSave < balance.innerText) {
         errorMsgSaving.style.display = 'none';
         lastBalance.innerText = remainBalance.toFixed(2);
     }
-   else{
-    errorMsg.style.display = 'none';  
-    errorMsgSaving.style.display = 'block';
-    savingAmount.innerText = 0;
-    lastBalance.innerText = 0;
-   }
+    else {
+        errorMsg.style.display = 'none';
+        errorMsgSaving.style.display = 'block';
+        savingAmount.innerText = 0;
+        lastBalance.innerText = 0;
+    }
 }
